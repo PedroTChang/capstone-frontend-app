718dd173d82a9ccd01fc38f0f0d10eaf232b948e
pedrotchang <28745927+pedrotchang@users.noreply.github.com>
2022-07-07 16:30:02 -0700

    adds homeview

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <nav>
     <router-link to="/">Home</router-link>
     |
-    <!-- <router-link to="/about">About</router-link> -->
-    <!-- <router-link to="/trackers">Tracker</router-link>
-    | -->
     <router-link to="/signup">Signup</router-link>
     |
     <router-link to="/login">Login</router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,12 @@ import HomeView from "../views/HomeView.vue";
 import SignupView from "../views/SignupView.vue";
 import LoginView from "../views/LoginView.vue";
 import LogoutView from "../views/LogoutView.vue";
-// import TrackersView from "../views/TrackersView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/signup", name: "signup", component: SignupView },
   { path: "/login", name: "login", component: LoginView },
   { path: "/logout", name: "logout", component: LogoutView },
-  // { path: "/trackers", name: "tracker", component: TrackersView },
   { path: "/", name: "home", component: HomeView },
   // { path: "/about", name: "about", component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue") },
 ];
