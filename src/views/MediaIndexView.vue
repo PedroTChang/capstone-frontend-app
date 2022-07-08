1c0b8b1de374cecac8247454f9ae0a9ffc149936
pedrotchang <28745927+pedrotchang@users.noreply.github.com>
2022-07-07 21:45:21 -0700

    gets all data in medium views from backend

--- src/views/MediaIndexView.vue
+++ src/views/MediaIndexView.vue
@@ -25,7 +25,11 @@ export default {
   <div class="media-index">
     <h1>Media</h1>
     <div v-for="medium in media" v-bind:key="medium.id">
-      <h2>{{ medium.name }}</h2>
+      <h2>Name: {{ medium.name }}</h2>
+      <h3>Latest:{{ medium.latest }}</h3>
+      <h4>Type: {{ medium.media_type }}</h4>
+      <h5>Status: {{ medium.status }}</h5>
+      <h6>Images: {{ medium.images }}</h6>
     </div>
   </div>
 </template>
