934908a01da55a9e03ed32b45eec4fa11b6c5de5
pedrotchang <pedrochang91@gmail.com>
2022-07-11 12:06:40 -0700

    adds synopsis to mediashow

--- src/views/MediaShowVue.vue
+++ src/views/MediaShowVue.vue
@@ -22,7 +22,8 @@ export default {
   <div class="media-show">
     <h1>{{ medium.name }}</h1>
     <img v-bind:src="medium.images[0].url" v-bind:alt="images.id" />
-    <!-- <div>{{ medium.description }}</div> -->
+    <h3>{{ medium.media_type }}</h3>
+    <div>{{ medium.synopsis }}</div>
     <router-link to="/media/">Back to media</router-link>
   </div>
 </template>
