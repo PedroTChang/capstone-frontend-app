da75abded0fedb733353f3575e439efb49168ae5
pedrotchang <pedrochang91@gmail.com>
2022-07-13 22:47:48 -0700

    completes the show page!

--- src/views/MediaShowVue.vue
+++ src/views/MediaShowVue.vue
@@ -14,17 +14,108 @@ export default {
       this.medium = response.data;
     });
   },
-  methods: {},
+  methods: {
+    createTracker() {
+      this.$emit("Successfully Added");
+    },
+  },
 };
 </script>
+
 <template>
-  <div class="media-show">
-    <h1>{{ medium.name }}</h1>
-    <img v-bind:src="medium.images[0].url" v-bind:alt="images.id" />
-    <h3>{{ medium.media_type }}</h3>
-    <div>{{ medium.synopsis }}</div>
-    <router-link to="/media/">Back to media</router-link>
+  <div class="container mt-100 mt-60">
+    <div class="row justify-content-center">
+      <div class="col-12">
+        <div class="section-title text-center mb-4 pb-2">
+          <h6 class="text-primary"></h6>
+          <h4 class="title mb-3 mt-2">{{ medium.name }}</h4>
+        </div>
+      </div>
+      <!--end col-->
+    </div>
+    <!--end row-->
+
+    <div class="row justify-content-center my-4 py-2">
+      <div class="col-md-6">
+        <img
+          v-bind:src="medium.images[0].url"
+          v-bind:alt="images.id"
+          class="img-fluid"
+        />
+      </div>
+      <!--end col-->
+    </div>
+    <!--end row-->
+
+    <div class="row justify-content-center">
+      <div class="col-lg-3 col-md-6 mt-4 pt-2">
+        <div class="text-center">
+          <h5 class="text-muted mb-0"></h5>
+          <h2 class="mb-0 display-5 mt-4 fw-bold text-primary">
+            <span class="counter-value" data-target="2021"
+              ><router-link class="btn btn-primary btn-lg" router-link to="/"
+                >Track</router-link
+              ></span
+            >
+          </h2>
+        </div>
+      </div>
+      <!--end col-->
+
+      <div class="col-lg-3 col-md-6 mt-4 pt-2">
+        <div class="text-center">
+          <h5 class="text-muted mb-0">Media Type</h5>
+          <h2 class="mb-0 display-5 mt-4 fw-bold text-primary">
+            <span class="counter-value" data-target="1000">{{
+              medium.media_type
+            }}</span>
+          </h2>
+        </div>
+      </div>
+      <!--end col-->
+
+      <div class="col-lg-3 col-md-6 mt-4 pt-2">
+        <div class="text-center">
+          <h5 class="text-muted mb-0">Current Release</h5>
+          <h2 class="mb-0 display-5 mt-4 fw-bold text-primary">
+            <span class="counter-value" data-target="10">{{
+              medium.latest
+            }}</span>
+          </h2>
+        </div>
+      </div>
+
+      <!--end col-->
+
+      <div class="col-lg-3 col-md-6 mt-4 pt-2">
+        <div class="text-center">
+          <h5 class="text-muted mb-0"></h5>
+          <h2 class="mb-0 display-5 mt-4 fw-bold text-primary">
+            <span class="counter-value" data-target="410"
+              ><router-link class="btn btn-primary btn-lg" router-link to="/"
+                >Back</router-link
+              ></span
+            >
+          </h2>
+        </div>
+      </div>
+      <p
+        class="text-muted para-desc mx-auto mb-0"
+        style="
+          padding-top: 50px;
+          padding-right: 20px;
+          padding-bottom: 200px;
+          padding-left: 20px;
+        "
+      >
+        {{ medium.synopsis }}
+      </p>
+
+      <!--end col-->
+    </div>
+    <!--end row-->
   </div>
+  <!--end container-->
 </template>
 
 <style></style>
